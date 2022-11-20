c3d9b41550f17c05f568180d8da3c05b41ef8015
Username <daniellesmikle@Danielles-Air.home>
2022-11-19 23:55:31 -0500

    register

--- gigi/admin.py
+++ gigi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Collection, Feature
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    list_display =['scent','description','slug','price','in_stock', 'created']
--- gigi/models.py
+++ gigi/models.py
@@ -16,7 +16,7 @@ class Feature(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/')
     slug = models.SlugField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
