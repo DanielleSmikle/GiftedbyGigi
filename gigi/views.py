56d293291749567588bcd554aae444335b2c2f4b
Username <daniellesmikle@Danielles-Air.home>
2022-12-12 23:16:46 -0500

    bootstrap

--- gigi/views.py
+++ gigi/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import *
+from .models import Customer, Collection, Feature, Order, OrderItem, ShippingAddress 
 
-
+def collections(request):
+    return {
+        'collections': Collection.objects.all()
+    }
 def all_features(request):
     features = Feature.objects.all()
     return render(request, 'gigi/home.html', {'features': features})
