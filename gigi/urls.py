8ac338c11b7e5753c40c192926e65c555ec4d5ed
Username <daniellesmikle@Danielles-Air.home>
2022-12-29 00:23:00 -0500

    psql

--- gigi/urls.py
+++ gigi/urls.py
@@ -6,6 +6,6 @@ app_name = 'gigi'
 urlpatterns = [
     path('', views.feature_all, name='feature_all'), 
     path('<slug:slug>', views.feature_detail, name='feature_detail'),
-    path('search/<slug:collection_slug>/', views.collection_list, name='collection_list'),
+    path('shop/<slug:collection_slug>/', views.collection_list, name='collection_list'),
 
 ]
