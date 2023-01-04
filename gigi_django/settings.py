1f3638f2b5659586854b4e81f8d306cca0b4a9b5
Username <daniellesmikle@Danielles-Air.home>
2023-01-03 19:24:39 -0500

    issues

--- gigi_django/settings.py
+++ gigi_django/settings.py
@@ -66,8 +66,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'gigi.context_processors.collections',
-                
-                
             ],
         },
     },
